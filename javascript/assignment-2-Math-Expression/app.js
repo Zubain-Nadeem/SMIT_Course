2eb01512ba07a52862e7b69478b8a5c620db5585
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-07 11:13:11 +0500

    Assignment 2 Math Expression

---
+++ javascript/assignment-2-Math-Expression/app.js
@@ -0,0 +1,207 @@
+// task no 1
+
+// let number1 = 13;
+// let number2 = 9;
+
+// let sum = number1+number2;
+// let sub = number1-number2;
+// let multi = number1*number2;
+// let dev = number1/number2;
+// let mod = number1%number2;
+
+// alert("Sum of 13 and 9 is" + " " + sum);
+
+// alert("Sum of 13 and 9 is" + " " + sub);
+
+// alert("Sum of 13 and 9 is" + " " + multi );
+
+// alert("Sum of 13 and 9 is" + " " + dev);
+
+// alert("Sum of 13 and 9 is" + " " + mod );
+
+// task no 1
+
+// task no 2
+
+// let number ;
+
+// document.write("Value after decleration is undefined" + "<br />")
+
+// let initialValue = 3;
+
+// document.write("Initial Value: " , initialValue + "<br />");
+
+// let increment = initialValue + 1;
+
+// document.write("Value after increment is ", increment + "<br />");
+
+// let addition = increment + 7;
+
+// document.write("Value after addition is ", addition + "<br />");
+
+// let decrement = addition - 1;
+
+// document.write("Value after decrement is ", decrement + "<br />")
+
+// let reminder = decrement % 3;
+
+// document.write("The reminder is ", reminder  + "<br />")
+
+// task no 2
+
+
+// task no 3
+// let ticketPrice = 600;
+// let quantity = 5 ;
+
+// let total = ticketPrice * quantity;
+
+// document.write("The cost to buy 5 tickes to a movie is " , total + "PKR .")
+
+// task no 3
+
+// task no 4
+
+// let table= +prompt("enter a table no") ;
+// let count = 1;
+
+
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+// document.write( table + " x " + count + " = " + table * count++ + "<br/>");
+
+// task no 4
+
+// task no 5
+
+// let item1 = 560;
+// let item2 = 200;
+
+// let quantity1 = 2;
+// let quantity2 = 5;
+
+// let subTotal1 = item1 * quantity1;
+// let subTotal2 = item2 * quantity2;
+
+// let shipCharge = 150;       
+// let grandTotal = subTotal1 + subTotal2 + shipCharge;
+
+// document.write("Price of item 1 is " , item1 + "<br />")
+// document.write("Quantity of item 1 is " , quantity1 + "<br />")
+// document.write("Price of item 2 is " , item2 + "<br />")
+// document.write("Quantity of item 2 is " , quantity2 + "<br />")
+
+// document.write("Shipping Charges " , shipCharge  + "<br />")
+
+// document.write("Total cost of your order is " , grandTotal + "PKR" )
+
+// task no 5
+
+// task no 6
+
+// let totalNumber = 1200 ;
+// let obtainedNumber = 900;
+// let percentage = (obtainedNumber/totalNumber) * 100;
+
+// document.write("Total Marks : " , totalNumber + "<br />");
+// document.write("Obtained Marks : " , obtainedNumber + "<br />");
+// document.write("Percentage : " , percentage + "%");
+
+// task no 6
+
+// task no 7
+
+// let usd = 10;
+// let riyal = 25;
+
+// let usdToPkr = 104;
+// let riyalToPkr = 28 ;
+
+// let total = (usd * usdToPkr) + (riyal * riyalToPkr);
+
+// document.write("Total Currency in PKR : ", total);
+
+// task no 7
+
+// task no 8
+
+// let num = 31;
+
+// let result = ((num + 5) * 10) / 2;
+
+// alert(result)
+
+// task no 8
+
+// task no 9
+
+// let currentYear = 2025;
+// let birthYear = +prompt("Enter your birth year");
+
+// let age1 = currentYear - birthYear;
+
+// let age2 = age1 - 1;
+
+// document.write("Current Year : " , currentYear + "<br />");
+// document.write("Birth Year : " , birthYear + "<br />");
+// document.write("They are either " , age1 + " or " , age2 + " years old");
+
+// task no 9
+
+
+// task no 
+
+// let radius = +prompt("Enter a radius");
+
+// let circum = 6.284 * radius;
+
+// let area = 3.142 * (radius * radius); 
+
+
+// document.write("Radius of Circle : " , radius + "<br />");
+// document.write("The Circumference is : " , circum + "<br />");
+// document.write("The area is : " , area + "<br />");
+
+
+// task no 10
+
+
+// task no 11 
+
+// let favSnack = prompt("Enter your favourite snack name ?");
+
+// let currentAge = +prompt("Enter your current age .");
+
+// let MaxAge = +prompt("Enter your maximum age you would like to eat your favourite snack .");
+
+// let snackAmout = +prompt("Enter Amount of snack per day .")
+
+// let total = (MaxAge - currentAge) * 365 * snackAmout;
+
+// document.write("Favourite Snack : " , favSnack + "<br />");
+// document.write("Current Age : " , currentAge + "<br />");
+// document.write("Estimated Maximum Age : " , MaxAge + "<br />");
+// document.write("Amount of snacks per day : " , snackAmout + "<br />" + "<br />");
+
+// document.write("You will need " , total +"  ", favSnack + " to last you until the ripe old age of " , MaxAge );
+
+// task no 11 
+
+
+
+
+
+
+
+
+
+
+
+
