f4cee3dce9bdf5b3bd6a62ffc267a78a4ba89fca
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-07 10:59:38 +0500

    If and Else Assignment

---
+++ javascript/assignment/app.js
@@ -0,0 +1,212 @@
+// IF and Else Assignment 
+
+// Task no 1 
+
+
+// let number = +prompt("Enter a num .");
+
+// if (number % 3 === 0) {
+//     console.log("Number is divided by 3");
+// }
+
+// else{
+//     console.log("Number is not devided by 3");
+    
+// }
+
+// Task no 1
+
+
+// Task no 2 
+
+// let number = +prompt("Enter a num .");
+
+// if (number % 2 === 0) {
+//     console.log("Even");
+// }
+
+// else{
+//     console.log("Odd");
+    
+// }
+
+// Task no 2 
+
+// Task no 3
+
+// let age = +prompt("Enter your age .")
+// if(age > 18 ){
+//     console.log("Old Enough");
+    
+// }
+
+// else{
+//     console.log("Too young ");
+// }
+
+// Task no 3
+
+// Task no 4
+
+// let number1 = +prompt("Enter a number .");
+
+// let number2 = +prompt("Enter a number .");
+
+// let operation = prompt("Enter a operation (E.g : + , - , *) .")
+
+
+// if(operation === "+"){
+//     console.log(number1 + number2);  
+// }
+
+// else if(operation === "-"){
+//     console.log(number1 - number2);  
+// }
+// else if(operation === "*"){
+//     console.log(number1 * number2);  
+// }
+// else if(operation === "/"){
+//     console.log(number1 / number2);  
+// }
+// else if(operation === "%"){
+//     console.log(number1 % number2);  
+// }
+
+// Task no 4
+
+// Task NO 5
+
+// let time = +prompt("Enter time in 24 hour formate (E.g : 1900 = 7pm .)");
+
+// if (time >="0000" && time<1200) {
+//     console.log("Good Morning .");
+// }
+// else if(time >= 1200 && time< 1700){
+//     console.log("Good Aternoon .")
+// }
+// else if(time >= 1700 && time< 2100){
+//     console.log("Good Evening .")
+// }
+// else if(time >= 2100 && time< 2359){
+//     console.log("Good Night .")
+// }
+
+// task no 5
+
+
+// Task no 6
+
+// let year = +prompt("Enter a year .");
+
+// if(year % 4 === 0 && year % 100 !== 0){
+//     console.log( year , " its a leap year .");
+// }
+
+// else{
+//     console.log(year ," Its not a leap year .")
+// }
+
+// Task no 6
+
+// Task no 7
+
+// let password = prompt("Please enter your password .")
+
+// if (password === "Zubain"){
+//     console.log("Correct! The password you entered matches the original password .");
+// }
+
+// else{
+//     console.log("Incorrect Pssword .")
+// }
+
+// task no 7
+
+// Task no 8
+
+// let firstName = prompt("Write your name .");
+
+// if (firstName === "Fahad"){
+//     document.write("Hello Fahad !")
+// }
+// else{
+//     document.write("You are not Fahad .")
+// }
+
+// Task no 8
+
+// Task no 9
+// let greeting ;
+// let hour = 13 ;
+
+// if(hour < 18 ){
+//     console.log(greeting , "Good Day .");   
+// }
+// else{
+//     console.log(greeting , "Good Evening .");
+    
+// }
+
+// Task no 9
+
+
+// Task no 10 
+
+// let num1 = +prompt("Enter first integer .");
+// let num2 = +prompt("Enter Second integer .");
+
+// if (num1 > num2) {
+//     console.log(num1 + " is larger then " + num2);
+// }
+// else if (num2 > num1) {
+//     console.log(num2 + " is larger then " + num1);   
+// }
+
+// else if (num2 === num1) {
+//     console.log("Both are equal .");   
+// }
+
+
+// Task no 10
+
+// Task no 11
+
+// let number = +prompt("Enter a number . ")
+
+// if(number > 0){
+//     console.log("its a positive no .");
+// }
+
+// else if(number < 0){
+//     console.log("its a negative no .");
+// }
+
+// else if(number === 0){
+//     console.log("its not a positive or nor a negative its zero .");
+    
+// }
+// Task no 11
+
+
+// Task no 12
+
+// let hour = +prompt("Enter current hour in 24 hour formate (E.g : 15 for 3pm and 6 for 6 am).")
+
+// if(hour >= 6 && hour <= 9){
+//     console.log("Its time for breakFast . ");
+// }
+
+// else if(hour >= 11 && hour <= 14 ){
+//     console.log("Its time for lunch .");  
+// }
+
+// else if(hour >= 17 && hour <= 20 ){
+//     console.log("Its time for dinner .");  
+// }
+// else{
+//     console.log("Sorry you will have to wait, or go get a snack .");
+    
+// }
+
+// Task no 12 
+ 
