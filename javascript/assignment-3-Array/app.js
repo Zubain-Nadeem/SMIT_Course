925ccd8db2886ea0a48a4958a54c3908283ddb40
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-11 11:43:37 +0500

    Array assignment

---
+++ javascript/assignment-3-Array/app.js
@@ -0,0 +1,115 @@
+// // Task no 1 store string array
+
+// let months = ["January" , "Febuary" , "March" , "Apirl" , "May" , "June"];
+// console.log(months);
+
+// // Task no 2 store number array 
+
+// let num = [1,2,3,4,6,7,8,9];
+// console.log(num);
+
+// // Task no 3 store boolean array 
+
+// let boolArray = [true , true , false , true ,false];
+// console.log(boolArray);
+
+// // Task no 4 store mix array 
+
+// let mixArray = ["January" , "Febuary" , 3,4,5,3,5, true , false ,false];
+// console.log(mixArray);
+
+// // Task no 5 store avaliable mobile networks in Pakistan.
+
+// let networks = ["Jazz" , "Zong" , "Ufone" , "Telenor"];
+// console.log(networks);
+
+// // Task no 6 Top movies of 2015
+
+// let topMovies = [];
+
+// document.write("<h1>Top movies of 2015 : <h1/>");
+
+// topMovies.push(`1) Avenger age of ultrone <br />`);
+
+// topMovies.push(`2) Avenger age of ultrone <br />`);
+
+// topMovies.push(`3) Avenger age of ultrone <br />`);
+
+// topMovies.push(`4) Avenger age of ultrone <br />`);
+
+// document.write(topMovies);
+
+// document.write(`Length of array : ${topMovies.length}`)
+
+
+// // Task no 7 Favourit cars 
+
+// let favCar = ["Audi" , "Aston Martin" , "G Wagon" , "Mercedes"];
+// let firstIndex = 0;
+// let lastIndex = 3 ;
+
+
+// document.write(`<h1> Favorite Cars <h1/>` );
+
+// document.write(`${favCar} <br/>`);
+
+// document.write(`The first index of array : ${firstIndex} <br />`);
+// document.write(`Car at first index of array : ${favCar[0]} <br />`);
+// document.write(`The last index of array : ${lastIndex} <br />`);
+// document.write(`Car at last index of array : ${favCar[3]} <br />`);
+
+// Task no 8 Display the score and percentage of student
+
+// let studentNames = ["Zubain" , "Ahmed" , "Qadir"];
+// let totatMarks = 500;
+// let obtainedMarks ;
+// let percentage ;
+
+// document.write(`Score of ${studentNames [0]} is ${obtainedMarks = 400} . ${percentage = obtainedMarks / totatMarks * 100} % <br />`);
+
+// document.write(`Score of ${studentNames [1]} is ${obtainedMarks = 360} . ${percentage = obtainedMarks / totatMarks * 100} % <br/>`);
+
+// document.write(`Score of ${studentNames [2]} is ${obtainedMarks = 290} . ${percentage = obtainedMarks / totatMarks * 100} % <br/>`);
+
+// Task no 9
+
+// let colors = ["Red" , "blue" , "Green" , "Yellow" + "<br />" ];
+// document.write(colors)
+
+// let addColorTostart = prompt("Which color do you want to add in start of array .");
+// colors.unshift(addColorTostart);
+// document.write(`Updated array after user added color in start : ${colors} <br />`) ; 
+
+// let addColorToLast = prompt("Which color do you want to add in last of array .");
+// colors.push(addColorToLast);
+// document.write(`Updated array after user added color in end : ${colors} <br />`) ; 
+
+// colors.unshift("White" , "Sink");
+// document.write(`Updated array after added two more color in start : ${colors} <br />`) ; 
+
+// colors.shift();
+// document.write(`Updated array after delete first color from start : ${colors} <br />`) ; 
+
+// colors.pop();
+// document.write(`Updated array after delete last color from end : ${colors} <br />`) ;
+
+// let userInd = +prompt("What index do you want to add the color ? (E.g : 0 , 1 , 2 ,3 )");
+// let userColo = prompt("What color do you want to add . ")
+
+// colors.splice(userInd , 0 , userColo);
+// document.write(`Updated array after added color according to your index number : ${colors} <br />`) ;
+
+// let userDel1 = +prompt("which index no color do you want to delete: ( E.g : 0 , 1 ,2,4 )");
+// let userDel2 = +prompt("Tell another index no color do you want to delete: ( E.g : 0 , 1 ,2,4 )");
+
+// colors.splice(userDel1 , userDel2);
+// document.write(`Updated array after you delete index color  : ${colors} <br />`) ;
+
+// Task no 9
+
+
+
+
+
+
+
