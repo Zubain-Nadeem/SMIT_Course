39c4793f2248304827e20482b1e8a58ac3ccc0cb
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-20 11:16:41 +0500

    Task no 2 compeleted

--- javascript/assignment-5-UserInput-Condition/app.js
+++ javascript/assignment-5-UserInput-Condition/app.js
@@ -1,5 +1,15 @@
 // Task no 1 
 
-let user = prompt("What is your name ?");
+// let user = prompt("What is your name ?");
 
-alert(`Welcom , ${user}`);
+// alert(`Welcom , ${user}`);
+
+// Task no 2 
+
+// let table = +prompt("Enter a table number do you want to multiply ." , "5");
+// let tableLen = +prompt("Enter number how many times do you want to muliply the number .");
+
+
+// for(let i = 1 ; i <= tableLen ; i++){
+//     document.write(`${table} x ${i} = ${table * i} <br />`)
+// }
