b86c6e45dd6f389b6026fa9f5db6648f54c81fa3
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-20 11:24:37 +0500

    Task no 3 compeleted

--- javascript/assignment-5-UserInput-Condition/app.js
+++ javascript/assignment-5-UserInput-Condition/app.js
@@ -13,3 +13,15 @@
 // for(let i = 1 ; i <= tableLen ; i++){
 //     document.write(`${table} x ${i} = ${table * i} <br />`)
 // }
+
+// Task no 3
+
+let city = prompt("Enter you city .");
+city = city.toLowerCase();
+
+let kar = "Karachi";
+kar = kar.toLowerCase();
+
+if(city === kar){
+    alert("Welcome to city of lights .")
+}
