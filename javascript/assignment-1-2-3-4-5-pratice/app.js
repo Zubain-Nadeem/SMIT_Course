b1db836faa4271ae58fc8aca35caf5599c8d03d9
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-02-24 12:04:52 +0500

    Three Pratice Question compeleted

---
+++ javascript/assignment-1-2-3-4-5-pratice/app.js
@@ -0,0 +1,70 @@
+// Pratice quesiton 1 "Concepts Used: Alerts, Variables, String Concatenation, Prompt"
+
+// let firtName = prompt("Enter you first name ?");
+// let lastName = prompt("Enter your last name ?");
+// let age = +prompt("Enter your age ?");
+
+// alert("Hello " + firtName + " " + lastName + " you are " + age + " years old");
+// alert(`Hello ${firtName} ${lastName} you are ${age} old .`);
+
+// Pratice question 2 Concepts Used: Math Expressions, Prompt, If-Else , basic calculator
+
+// let firstNumber = +prompt("Enter a number ?");
+// let secondNumber = +prompt("Enter another number ?");
+// let operator = prompt("Enter operatation do you want to performe .");
+// let result;
+
+// if (operator === "+") {
+//   result = firstNumber + secondNumber;
+//   document.write(`first number : ${firstNumber}
+//                  <br /> second number : ${secondNumber}
+//                  <br /> operator : ${operator}
+//                  <br /> result : ${result}`);
+// } 
+// else if (operator === "-") {
+//   result = firstNumber - secondNumber;
+//   document.write(`first number : ${firstNumber}
+//                    <br /> second number : ${secondNumber}
+//                    <br /> operator : ${operator}
+//                    <br /> result : ${result}`);
+// }
+//  else if (operator === "*") {
+//   result = firstNumber * secondNumber;
+//   document.write(`first number : ${firstNumber}
+//                    <br /> second number : ${secondNumber}
+//                    <br /> operator : ${operator}
+//                    <br /> result : ${result}`);
+// } 
+// else if (operator === "/") {
+//   result = firstNumber / secondNumber;
+//   document.write(`first number : ${firstNumber}
+//                    <br /> second number : ${secondNumber}
+//                    <br /> operator : ${operator}
+//                    <br /> result : ${result}`);
+// } 
+// else if (operator === "%") {
+//   result = firstNumber % secondNumber;
+//   document.write(`first number : ${firstNumber}
+//                    <br /> second number : ${secondNumber}
+//                    <br /> operator : ${operator}
+//                    <br /> result : ${result}`);
+// } else {
+//   document.write(`Wrong operator enter + - * / %`);
+// }
+
+
+// Pratice question 3 Concepts Used: If-Else, Math Expressions, Prompt
+
+// let num = +prompt("Enter a number ");
+
+// if (num % 2 === 0){
+//   document.write(`Number ${num} its a even number `);
+
+// }
+// else if (num % 2 !== 0){
+//   document.write(`Number ${num} its a odd number .`)
+// }
+
+// else{
+//   document.write(`its not a number `)
+// }
