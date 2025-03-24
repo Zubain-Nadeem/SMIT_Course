ddebb09e51d27acd0f7140a2a80181c71b073096
Zubainali23@gmail.com <zubainali23@gmail.com>
2025-03-25 02:19:10 +0500

    result on click

--- javascript/assignment-7-function/app.js
+++ javascript/assignment-7-function/app.js
@@ -50,3 +50,46 @@ function changeDice(){
 
     console.log(randomNum)
 } 
+
+
+
+let totalMarks = document.querySelectorAll(".total")[0];
+let obtMarks = document.querySelectorAll(".obtaind")[0];
+
+let hE1 = document.querySelectorAll(".totalNum")[0];
+let hE2 = document.querySelectorAll(".obtaindNum")[0];
+let hE3 = document.querySelectorAll(".percent")[0];
+let hE4 = document.querySelectorAll(".grade")[0];
+
+function result() {
+  hE1.innerHTML = `Total Marks : ${totalMarks.value}`;
+  hE2.innerHTML = `Obtaind Marks : ${obtMarks.value}`;
+  let percentage = (obtMarks.value / totalMarks.value) * 100;
+  hE3.innerHTML = `Percentage : ${percentage.toFixed(2)} %`;
+
+  if (percentage >= 80) {
+    hE4.innerHTML = ` Grade : A+ Excellent`;
+  } 
+  else if (percentage >= 70) {
+    hE4.innerHTML = ` Grade : A Very Good`;
+  } 
+  else if (percentage >= 60) {
+    hE4.innerHTML = ` Grade : B Good`;
+  }
+   
+  else if (percentage >= 50) {
+    hE4.innerHTML = ` Grade : D Better`;
+  } 
+  else if (percentage >= 35) {
+    hE4.innerHTML = ` Grade : E Do hard work`;
+  } 
+  else {
+    hE4.innerHTML = `Your are Fail`;
+    hE4.style.color = "red ";
+  }
+
+  totalMarks.value = "";
+  obtMarks.value = "";
+  return;
+}
+
